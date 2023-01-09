0bee10e1e38b9697639b382e9cc8136c22a20ab1
bobrosoft <bobrosoft@yandex.ru>
2023-01-09 21:15:27 +0400

    CARD-17: [DevOps] Move to new VPS (fixes)

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,5 +5,5 @@ set -e
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_PATH
 
-git pull
-sudo systemctl restart telegram-bot-crypto-daddy
+runuser -u www -- git pull
+systemctl restart telegram-bot-crypto-daddy
